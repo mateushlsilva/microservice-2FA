117744fdf3a23bb3d27c27e50e36edbd89a527b6
mateushlsilva <mateushls01@gmail.com>
2023-09-07 18:52:18 -0300

    Adiciona generate 2FA

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from models.index import connection
 from dotenv import load_dotenv
+from blueprint.generate2FA import generate
 load_dotenv()
 
 app = Flask(__name__)
@@ -11,5 +12,6 @@ import routes.index
 
 if __name__ == "__main__":
     connection()
+    generate()
     app.run(debug=True)
 
